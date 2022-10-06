84b81f801f5d6e901c3e789e997c675fe51fd05e
Rishabh anand <rishabhanand341@gmail.com>
2022-10-06 08:17:36 +0530

    e

--- sample_config.py
+++ sample_config.py
@@ -76,7 +76,7 @@ class Config(object):
     TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "THANOSBOT")
     # for custom thumb image set this with your required thumb telegraoh link
     THUMB_IMAGE = os.environ.get(
-        "THUMB_IMAGE", "https://telegra.ph/file/ca95524e4734b0d5461b5.jpg"
+        "THUMB_IMAGE", "https://telegra.ph/file/9e2f854ed7b16df90992d.jpg"
     )
     # specify NO_LOAD with plugin names for not loading in userbot
     NO_LOAD = list(os.environ.get("NO_LOAD", "").split())
