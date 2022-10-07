08954691beadea9f0ed5cd4f09b3563a656b6ce9
Notyourrichbaby <115189846+Notyourrichbaby@users.noreply.github.com>
2022-10-08 00:24:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,22 +3,22 @@ from sample_config import Config
 
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = 10248430
-    API_HASH = "42396a6ff14a569b9d59931643897d0d"
+    APP_ID = 4524654
+    API_HASH = "4cfbcd31ec888cc3f0709d8d20607ac8"
     # the name to display in your alive message
-    ALIVE_NAME = "rishabh"
+    ALIVE_NAME = "proboy"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "postgres://sphdgasx:XS6xp4XCyq2nO4awAr3GjTnfnuTYUYrK@satao.db.elephantsql.com/sphdgasx"
+    DB_URI = "postgres://qclzikru:rFsW1lbh6UoZcKzRvtBAouAgDk4jRtXj@jelani.db.elephantsql.com/qclzikru"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "1BVtsOLcBuyHRWi44e2v7PDIn-F-0cHhfPRVKEzAWzjomfEODbE4IRifTXW9a9Qrg4cFX1aSFCJhfAeg-8GWUVLS1V6mqk2yRB-VbjDa2suVmrnGPfp7HjpPOWSHECee3AKjHFX_nw6gSC_145D5KMWBfYFx-xL02sOeA0gtSZMwM8dUAClbiqTVAxS8x9-ujW4mah2hhomSzCm7c0uMNTdy3Mtuo6jaGEWbt3X8M4DribhQQttHzlzLLR8oICL9ru3ObbLIL4DkwLJtmlgAQcbKLmcO0hOpTzMeAydYhHJSHZZPKtuzrYk35_0ecS13lxfd7WjfH3vqtWIyL6M0WAZlRIBC0scg="
+    STRING_SESSION = "1BVtsOKwBu44jUXB0S3C_87B0h-bny9gsD6A8hjZ24QKx4dI2qWdqODxFr7EGENBIHCMSy6LBJ4n9iGgTqvfACfD6lyT3_kGALgyqKZ5xyq_9s86UrXrkBgYZ_vUMhi9q9eq5-SnLwc1fYhVFxtw_fg1wppa1wxG63BTKJMpCCgpQq55memYhS68fITKGiwjTjusqfOVJt4KVcPSeRCrxX4UNknUl5AKj0kMepfFj_2-c0Dd93QxwMmz_NhhCrQO0SAT2rZFwXKBGnCNppbZDFLf7sgGtF9XfPBzxuT1_lOr83jFiD--S7uSEXFagX7Kt3zJEVwDmpd3MhYVZ0lCZbtrIEMMxLEg="
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "5509473272:AAEXFx2yPYrY_KvL4B9lrn3Zh0jzhJWPaBs"
+    TG_BOT_TOKEN = "5527126428:AAFSGWSO_86OBjI6Y2VdrpmPro-6Ow9RF2I"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = -1001623978356
+    PRIVATE_GROUP_BOT_API_ID = -1001638681164
     # command handler
     COMMAND_HAND_LER = "."
     VCMODE = "True"
-    VC_SESSION = "1AZWarzwBu2-D4d54xwEvVk3RZWj3KNvOjg5R_wBa-tskXU0Ar5WvaMGABzQIgAWCnzLF_hIZVA-zlyBy5TeLNKqBGj_MddvQ7ZBhzJzAMNqxqq6QqB7EcdYLknN_v_PqB-I3r1NsnJhX8ISf5VIRwg2nBPOh2Kbar0BMPkNjXzNE8orgLKkuAXkdHg84YzAjv1nTKoBznL_9xjnWoCtd5TphbJa2wdw0npaKjq1jFBgmo3iCra3F8C7CgNfQRH3WrxMaemGwn8D8wafAy5CqrfEUCnN8xZvV8x-6twPtIIlCBD3BU3E-oWqoimlZ3KPREGHvs_U-XdTdM8EmAV7162SgcFpkKmo="
+    VC_SESSION = "1BVtsOKwBu44jUXB0S3C_87B0h-bny9gsD6A8hjZ24QKx4dI2qWdqODxFr7EGENBIHCMSy6LBJ4n9iGgTqvfACfD6lyT3_kGALgyqKZ5xyq_9s86UrXrkBgYZ_vUMhi9q9eq5-SnLwc1fYhVFxtw_fg1wppa1wxG63BTKJMpCCgpQq55memYhS68fITKGiwjTjusqfOVJt4KVcPSeRCrxX4UNknUl5AKj0kMepfFj_2-c0Dd93QxwMmz_NhhCrQO0SAT2rZFwXKBGnCNppbZDFLf7sgGtF9XfPBzxuT1_lOr83jFiD--S7uSEXFagX7Kt3zJEVwDmpd3MhYVZ0lCZbtrIEMMxLEg="
     # command hanler for sudo
     SUDO_COMMAND_HAND_LER = "."
     # External plugins repo
