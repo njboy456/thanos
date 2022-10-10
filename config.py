345753ddc81804a8f33897bb30b4c51cf927dcbd
rishabh Anand <115479204+thanospros@users.noreply.github.com>
2022-10-10 19:48:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,19 +6,20 @@ class Development(Config):
     APP_ID = 4524654
     API_HASH = "4cfbcd31ec888cc3f0709d8d20607ac8"
     # the name to display in your alive message
-    ALIVE_NAME = "proboy"
+    ALIVE_NAME = "Շђคภ๏ร"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "postgres://qclzikru:rFsW1lbh6UoZcKzRvtBAouAgDk4jRtXj@jelani.db.elephantsql.com/qclzikru"
+    DB_URI = "YOUR VALUE" # IF U DONT KNOW HOW TO MAKE THEN GO IN THANOS SUPPORT AND TYPE #DB_URI 
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "1BVtsOKwBu44jUXB0S3C_87B0h-bny9gsD6A8hjZ24QKx4dI2qWdqODxFr7EGENBIHCMSy6LBJ4n9iGgTqvfACfD6lyT3_kGALgyqKZ5xyq_9s86UrXrkBgYZ_vUMhi9q9eq5-SnLwc1fYhVFxtw_fg1wppa1wxG63BTKJMpCCgpQq55memYhS68fITKGiwjTjusqfOVJt4KVcPSeRCrxX4UNknUl5AKj0kMepfFj_2-c0Dd93QxwMmz_NhhCrQO0SAT2rZFwXKBGnCNppbZDFLf7sgGtF9XfPBzxuT1_lOr83jFiD--S7uSEXFagX7Kt3zJEVwDmpd3MhYVZ0lCZbtrIEMMxLEg="
-    # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "5527126428:AAFSGWSO_86OBjI6Y2VdrpmPro-6Ow9RF2I"
+    STRING_SESSION = "YOUR VALUE"# IF U DONT KNOW HOW TO MAKE THEN GO IN THANOS SUPPORT AND TYPE #session 
+    # --------------------------------------------------
+
+    TG_BOT_TOKEN = "Your value"# create a new bot in @botfather and fill the following vales with bot token
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
     PRIVATE_GROUP_BOT_API_ID = -1001638681164
     # command handler
     COMMAND_HAND_LER = "."
-    VCMODE = "True"
-    VC_SESSION = "1BVtsOKwBu44jUXB0S3C_87B0h-bny9gsD6A8hjZ24QKx4dI2qWdqODxFr7EGENBIHCMSy6LBJ4n9iGgTqvfACfD6lyT3_kGALgyqKZ5xyq_9s86UrXrkBgYZ_vUMhi9q9eq5-SnLwc1fYhVFxtw_fg1wppa1wxG63BTKJMpCCgpQq55memYhS68fITKGiwjTjusqfOVJt4KVcPSeRCrxX4UNknUl5AKj0kMepfFj_2-c0Dd93QxwMmz_NhhCrQO0SAT2rZFwXKBGnCNppbZDFLf7sgGtF9XfPBzxuT1_lOr83jFiD--S7uSEXFagX7Kt3zJEVwDmpd3MhYVZ0lCZbtrIEMMxLEg="
+    #VCMODE = "True" coming soon
+    #VC_SESSION = "-c0Dd93QxwMmz_NhhCrQO0SAT2rZFwXKBGnCNppbZDFLf7sgGtF9XfPBzxuT1_lOr83jFiD--S7uSEXFagX7Kt3zJEVwDmpd3MhYVZ0lCZbtrIEMMxLEg="
     # command hanler for sudo
     SUDO_COMMAND_HAND_LER = "."
     # External plugins repo
