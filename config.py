206dcee9ea7b06e7255ea52093b289043215191a
njboy456 <115774985+njboy456@users.noreply.github.com>
2022-10-14 15:09:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Development(Config):
     # the name to display in your alive message
     ALIVE_NAME = "NJ BOY"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "YOUR VALUE" # IF U DONT KNOW HOW TO MAKE THEN GO IN THANOS SUPPORT AND TYPE #DB_URI 
+    DB_URI = "postgres://bsjthsmg:4bGSCNBJtm08RuycnsoPEns0EeN_sflP@otto.db.elephantsql.com/bsjthsmg" # IF U DONT KNOW HOW TO MAKE THEN GO IN THANOS SUPPORT AND TYPE #DB_URI 
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
     STRING_SESSION = "1BVtsOIgBuyxze5MUs2JRp7wYmQi_dX9FndS3CHljHvu__LVLM3mBiKof6bXRgdX0AW2Ae7QTLtF01x4K5q3FQYIpkE_X9Sm8wcgRaSk6PUmdPBF6S6ByqYx78_oeyNu3omDuetZsGroQVsRcgKg4a5QdJU5fwePC8KQBHrqUQbXeMIaksW9cXFEQBI1KZQO0McWN_MX3QprjgE_-Xs6lCTywtMBGwwG6Yrkls3gNRQMvstAf0vOmmZstx0fT-fLAbOQ9TDqcJdd3BVvKqJ-wD7AEvEgTUS7re3DZLrZthjOXxXxN8-5cvet94z2WCEy9q7EnI-1kks1NzbJ56W731otONHXDRDE="# IF U DONT KNOW HOW TO MAKE THEN GO IN THANOS SUPPORT AND TYPE #session 
     # --------------------------------------------------
@@ -19,7 +19,7 @@ class Development(Config):
     # command handler
     COMMAND_HAND_LER = "."
     VCMODE = "True"
-    VC_SESSION = ""
+    VC_SESSION = "AQC7-xGlSwNv0YMCTjzuWgNuQSL5n6sSMiXlyPa-gbGAaIOBsOqH6XfaZccyHhYYWdkNiDnPCjFsmK18vvh5YVLYq2HFljCFHp61R8pjWnXTl1LnUdOtmb7xszk7r4zVqsDvwxPjY-kMFGY263HoM4Od3IAIySvCM8kV3WitS-JpVJt5DKMiaD8LnMGebdxUyJjHMrceZmsi082UQbv7Vau5bW_V4HZFSS9WQSdklpYndb1ISrUljwXswfatRZD2M5HmB8yP9iRyHEsgDxB9-Al7o25-nZKHbgBwgg85x0pV2pZ5Z0ENRIcLfbANHIO83h_HrpPr7vik0nr5DfTkfuJJAAAAAUoiaDcA"
     # command hanler for sudo
     SUDO_COMMAND_HAND_LER = "."
     # External plugins repo
